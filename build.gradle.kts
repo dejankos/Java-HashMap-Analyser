029c2e1150efa896a42d3e77540334a57704a0e3
Dejan Kos <kosdejan@yahoo.com>
2023-02-26 11:29:05 +0100

    Switch to gradle (#3)

---
+++ build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    java
+    jacoco
+}
+
+group = "io.github.dejankos"
+version = "1.1-SNAPSHOT"
+description = "hash-map-analyser"
+java.sourceCompatibility = JavaVersion.VERSION_1_8
+
+repositories {
+    mavenLocal()
+    maven {
+        url = uri("https://repo.maven.apache.org/maven2/")
+    }
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.4.2")
+}
+
+jacoco {
+    toolVersion = "0.8.8"
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.required.set(true)
+        csv.required.set(true)
+    }
+    dependsOn(tasks.test)
+}
