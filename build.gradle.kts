cab971383b3e55d59b06d95f424b96d5f5b7e303
Dejan Kos <dkos@gradle.com>
2023-02-26 10:20:10 +0100

    jc

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,3 +33,10 @@ tasks.jacocoTestReport {
         csv.required.set(true)
     }
 }
+
+tasks.test {
+    finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
+}
+tasks.jacocoTestReport {
+    dependsOn(tasks.test) // tests are required to run before generating the report
+}
